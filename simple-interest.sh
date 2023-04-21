906a55e6814ea505eda2452539b636e580b8411a
priya270497 <131447325+priya270497@users.noreply.github.com>
2023-04-21 15:12:45 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# priya270497
 # Input:
 # p, principal amount
 # t, time period in years
